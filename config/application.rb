cbde6b4cb80c284b5f1ee9c641f8f844018ec7fd
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-12-04 11:25:36 +0000

    config

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Menzuki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    # config.load_defaults 6.1
+    config.load_defaults 5.2
     
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
