0c7a038ea2b833516fee184f2a18f017ea01f875
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-12-08 13:16:32 +0000

    layout_fix

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -8,7 +8,7 @@ before_action :authenticate_member!
 
   def create
     #レビューの新規投稿
-    @review= Review.new(review_params)
+    @review = Review.new(review_params)
     #@reviewに紐づくscoreとして値を格納
     @review.score = Language.get_data(review_params[:review_cont])
     if @review.save
@@ -30,9 +30,8 @@ before_action :authenticate_member!
   end
 
   def destroy
-    @review= Review.find(params[:id])
-    if @review.destroy
-      @reviews= Review.all.order(created_at: :desc).page(params[:page]).per(4)
+    review = Review.find(params[:id])
+    if review.destroy
       redirect_to reviews_path
     end
   end
