5e30244dc1c0ebf339fc78f73eb8c5b12b98ef3b
EC2 Default User <ec2-user@ip-172-31-42-82.ap-northeast-1.compute.internal>
2023-01-02 11:53:10 +0000

    fix

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,25 +2,16 @@ class Public::FavoritesController < ApplicationController
 
  before_action :authenticate_member!
 
-def create  #いいねする
+def create
  @review = Review.find(params[:review_id])
-#いいねをする対象の投稿idを取得
  favorite = current_member.favorites.new(review_id: @review.id)
-#現在のmemberに紐づくfavoriteテーブルのレコードを作成
-#作成するレコードのreview_idカラムは上記で取得したいいねの対象となる投稿id
  favorite.save
-#レコード保存
- # redirect_back fallback_location: root_path
 end
 
-def destroy #いいねを取り消す
+def destroy
  @review = Review.find(params[:review_id])
-#いいねを取り消す対象の投稿idを取得
  favorite = current_member.favorites.find_by(review_id: @review.id)
-#現在のmemberに紐づくfavoriteテーブル(review_idカラムが上記で取得したいいね取り消しの対象となる投稿idのもの)
  favorite.destroy
-#レコード削除
- # redirect_back fallback_location: root_path
 end
 
 
